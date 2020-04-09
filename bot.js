71fd01f7628a609ed3ac71ca6b10ab394f82b9fe
Peggy Soh <peggy.soh@live.com>
2020-04-08 22:15:21 -0500

    Squashed commit of the following:
    run it once

--- bot.js
+++ bot.js
@@ -5,25 +5,28 @@ const client = new Client();
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
+  await getEvents();
+  setInterval(getEvents, 60000 * 60 * 24); // 1 min *  every hour
+});
+
+async function getEvents() {
   const channel = client.channels.cache.find((c) => c.name === 'announcements');
 
-  setInterval(async function () {
-    await axios
-      .get('https://nookipedia.com/api/today/', {
-        headers: {
-          'x-api-key': process.env.API_KEY
-        }
-      })
-      .then(function (response) {
-        let message = `**${response.data.message}**`;
-        response.data.events.forEach((e) => {
-          message += `\n• ${e}`;
-        });
-        channel.send(message, {
-          files: response.data.villager_images
-        });
+  await axios
+    .get('https://nookipedia.com/api/today/', {
+      headers: {
+        'x-api-key': process.env.API_KEY
+      }
+    })
+    .then(function (response) {
+      let message = `**${response.data.message}**`;
+      response.data.events.forEach((e) => {
+        message += `\n• ${e}`;
       });
-  }, 60000 * 60 * 24); // 1 min *  every hour
-});
+      channel.send(message, {
+        files: response.data.villager_images
+      });
+    });
+}
 
 client.login(process.env.BOT_TOKEN);
